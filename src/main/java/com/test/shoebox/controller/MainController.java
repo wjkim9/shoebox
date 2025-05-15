8d0542a40e4b20b49134b48a23a0fda2003e7fbc
jinin1346 <thdnjs1346@naver.com>
2025-05-15 17:38:24 +0900

    0515
    0515

--- src/main/java/com/test/shoebox/controller/MainController.java
+++ src/main/java/com/test/shoebox/controller/MainController.java
@@ -8,11 +8,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/main")
 public class MainController {
-	
+
 	@GetMapping("/")
-	public String index(Model model) {
-		
-		return "main/index";
+	public String main(Model model) {
+
+		return "main/main";
 	}
-	
+
+	@GetMapping("/login")
+	public String login(Model model) {
+
+		return "main/login";
+	}
+
+	@GetMapping("/findid")
+	public String findid(Model model) {
+
+		return "main/findid";
+	}
+
+	@GetMapping("/findpw")
+	public String findpw(Model model) {
+
+		return "main/findpw";
+	}
+
+	@GetMapping("/register")
+	public String register(Model model) {
+
+		return "main/register";
+	}
+
 }
