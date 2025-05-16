0e9352f9b7553e7e6707de3b4aba0e6eb69c8806
BroYears <micael456@naver.com>
2025-05-16 15:12:02 +0900

    2025-05-16
    main 수정

--- src/main/java/com/test/shoebox/controller/main/MainController.java
+++ src/main/java/com/test/shoebox/controller/main/MainController.java
@@ -8,12 +8,36 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/main")
 public class MainController {
-	
+
+
 	@GetMapping("/")
-	public String index() {
+	public String main(Model model) {
 
-
-		return "main/index";
+		return "main/main";
 	}
-	
+
+	@GetMapping("/login")
+	public String login(Model model) {
+
+		return "main/login";
+	}
+
+	@GetMapping("/findid")
+	public String findid(Model model) {
+
+		return "main/findid";
+	}
+
+	@GetMapping("/findpw")
+	public String findpw(Model model) {
+
+		return "main/findpw";
+	}
+
+	@GetMapping("/register")
+	public String register(Model model) {
+
+		return "main/register";
+	}
+
 }
